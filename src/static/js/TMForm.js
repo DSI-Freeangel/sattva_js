6efc51f06fc282d7b507ed8501c09145d1b2e621
freangel <ilua.moskaluk@gmail.com>
2016-03-21 07:56:52 +0200

    fix static texts

--- src/static/js/TMForm.js
+++ src/static/js/TMForm.js
@@ -173,8 +173,8 @@ $(window).load(function(){
 								.removeClass(opt.processingClass)
 								.addClass(opt.successClass)
 
-							modal.find('.modal-title').text('Success!')
-							modal.find('.modal-body').text('Your message has been successfully sent!')
+							modal.find('.modal-title').text('Успішно')
+							modal.find('.modal-body').text('Ваше повідомлення успішно відправлено!')
 
 							setTimeout(function(){
 								form
